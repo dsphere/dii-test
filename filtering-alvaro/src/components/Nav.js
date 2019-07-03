4e6f27271e8d1950787eca9d3a2170aff7c9d73a
Alvaro De La Torre <alvarodltp@gmail.com>
2019-07-03 09:43:10 -0400

    adds logo on nav

--- filtering-alvaro/src/components/Nav.js
+++ filtering-alvaro/src/components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Image } from 'semantic-ui-react'
 
 const Nav = () => {
   return(
     <div className="nav">
-      
+      <Image className="logo" src={'/logo.png'} />
     </div>
   )
 }
